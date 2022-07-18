7ecda852fda330a2ba2ffb8e868e39ed14543303
EricSzakvary <eric.szakvary.pro@gmail.com>
2022-07-18 18:27:29 +0200

    [code, graph] basic turret + water shader

---
+++ Assets/Scripts/CS_Turret.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Turret : MonoBehaviour
+{
+    List<CS_Unit> listUnit;
+
+    Coroutine mainCoroutine;
+
+    private void Awake()
+    {
+        listUnit = new List<CS_Unit>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(listUnit.Count);
+        Debug.Log(mainCoroutine);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Unit"))
+        {
+            listUnit.Add(other.gameObject.GetComponent<CS_Unit>());
+
+            UpdateCoroutine();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Unit"))
+        {
+            listUnit.Remove(other.gameObject.GetComponent<CS_Unit>());
+
+            UpdateCoroutine();
+        }
+    }
+
+    void UpdateCoroutine()
+    {
+        if (listUnit.Count > 0 && mainCoroutine == null)
+        {
+            mainCoroutine = StartCoroutine(TimerFire());
+        }
+        else if (listUnit.Count == 0 && mainCoroutine != null)
+        {
+            Debug.Log("stop coroutine");
+            StopCoroutine(mainCoroutine);
+            mainCoroutine = null;
+        }     
+    }
+    
+    IEnumerator TimerFire()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(2f);
+            KillUnit();
+            Debug.Log("kill");
+        }  
+    }
+
+    void KillUnit()
+    {
+        Destroy(listUnit[0].gameObject);
+        listUnit.RemoveAt(0);
+
+        UpdateCoroutine();
+    }
+}
